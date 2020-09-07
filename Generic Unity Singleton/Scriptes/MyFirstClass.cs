73d6cf7fd6c2e3e3e922779e14a562d7a7189ea7
RAHUL KUMAR RAWAT <25106490+Rahulkurawat@users.noreply.github.com>
2020-09-07 12:00:01 +0530

    Add files via upload

---
+++ Generic Unity Singleton/Scriptes/MyFirstClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstClass : GenericSingletonClass<MyFirstClass> 
+{
+    public int numbers = 100;
+
+    public string textline = "I am first class";
+  
+    // Start is called before the first frame update
+  
+    void Start()
+    {
+        MySecundClass.Instance.Text3.text = "This Text is set from First Class";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Generic Unity Singleton/Scriptes/MySecundClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MySecundClass : GenericSingletonClass<MySecundClass>
+{
+    public Text Text1, Text2, Text3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text1.text = MyFirstClass.Instance.numbers.ToString() +" Set from scound class";
+        print("Starting Value of the number in First Class is : "+MyFirstClass.Instance.numbers);
+
+        Text2.text = MyFirstClass.Instance.textline.ToString() + " Set from scound class";
+
+        print("Starting Value of the String text in First Class is : " + MyFirstClass.Instance.textline);
+
+        MyFirstClass.Instance.numbers += 100;
+
+        print("After change Value of the number in First Class is : " + MyFirstClass.Instance.numbers);
+    }
+
+    void Update()
+    {      
+
+    }
+}
